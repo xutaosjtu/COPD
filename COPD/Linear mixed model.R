ffe005eb2f8f149111d4993ef73163af911f7000
xutaosjtu <xutaosjtu@gmail.com>
2012-10-07 22:14:37 +0200

    recaluculated

--- COPD/Linear mixed model.R
+++ COPD/Linear mixed model.R
@@ -11,14 +11,14 @@ model2 = c(model1, "PACKYEARS")
 model3 = c(model2, "FEV1_SCR_PRE","FEV1FVC_SCR_PRE")
 
 metabolites = colnames(data.normalized)
-data = data.frame( COPD = COPD.data$COPD, COPD.data[, c(model1, "visit", "id", "exercise")])
+data = data.frame( COPD = COPD.data$COPD, COPD.data[, c(model3, "visit", "id", "exercise")])
 
 rst.Disease = NULL
 rst.interaction = NULL
 for(metabo in metabolites){
-	metabo = data.normalized[ which(data$exercise == 3),metabo] - data.normalized[ which(data$exercise == 4),metabo]
-	
-	model = lme( metabo ~ COPD * SEX  + AGE + BMI, #+ PACKYEARS + FEV1_SCR_PRE + FEV1FVC_SCR_PRE, 
+	#metabo = data.normalized[ which(data$exercise == 3),metabo] - data.normalized[ which(data$exercise == 1),metabo]
+	metabo = data.normalized[ which(data$exercise == 1),metabo]
+	model = lme( metabo ~ COPD * SEX  + AGE + BMI,#+ PACKYEARS,# + FEV1_SCR_PRE + FEV1FVC_SCR_PRE, 
 			data = data[which(data$exercise == 1),],
 			na.action = na.omit,
 			random = ~1|id/visit, 
@@ -31,6 +31,8 @@ for(metabo in metabolites){
 }
 rownames(rst.Disease) = metabolites;
 rownames(rst.interaction) = metabolites;
+rst.Disease$FDR = p.adjust (rst.Disease$`p-value`, method = "BH")
+rst.interaction$FDR = p.adjust (rst.interaction$`p-value`, method = "BH")
 
-write.csv(rst.Disease, file = "disease differences TP4_TP3_model 3.csv")
-write.csv(rst.interaction, file = "disease sex interaction at TP4_TP3_model 3.csv")
+write.csv(rst.Disease, file = "disease differences baseline_model 1.csv")
+write.csv(rst.interaction, file = "disease sex interaction at baseline_model 1.csv")
