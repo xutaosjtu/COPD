330d9a7a2440db9ba1f177766b7ed4588f416ae9
Tao Xu <xutaosjtu@gmail.com>
2012-10-10 10:15:42 +0200

    nothing changed

--- COPD/PLS analysis.R
+++ COPD/PLS analysis.R
@@ -18,11 +18,13 @@ require(gplots)
 data = data.frame(data.normalized, COPD = as.numeric(COPD.data$COPD))
 
 S4pls<-plsr(COPD ~ . , data=data, subset = which(COPD.data$exercise == 3))
+#S4pls<-plsda(x=data.normalized, COPD.data$COPD, ncomp = 10)
+
 
 #color = rep(0, dim(data)[1])
 color = greenred(24)[c(c(4:1)*2,c(18,20, 22, 24)) ]
-plot(S4pls$scores[,1:3],col=color[c(4,8)][interaction(COPD.data$exercise[which(COPD.data$exercise == 1)],COPD.data$COPD[which(COPD.data$exercise == 1)])], pch=c(17, 19)[COPD.data$SEX[which(COPD.data$exercise == 1)]], main = "PLS plot of COPD vs non COPD")
-#plot(S4pls$scores[,1:2],col = color[data$COPD[which(COPD.data$exercise == 1)]], pch=c(17, 19)[COPD.data$SEX[which(COPD.data$exercise == 1)]])
+#plot(S4pls$scores[,1:2],col=color[c(4,8)][interaction(COPD.data$exercise[which(COPD.data$exercise == 1)],COPD.data$COPD[which(COPD.data$exercise == 1)])], pch=c(17, 19)[COPD.data$SEX[which(COPD.data$exercise == 1)]], main = "PLS plot of COPD vs non COPD")
+plot(S4pls$scores[,c(1,2)],col = color[interaction(COPD.data$exercise,COPD.data$COPD)], pch=c(17, 19)[COPD.data$SEX[which(COPD.data$exercise == 1)]])
 
 
 #PLS of exercises
@@ -40,8 +42,8 @@ S4pls<-plsr(exercise_COPD ~ . , data=data)
 
 plot(S4pls$scores[,1:3],col=color[data$exercise_COPD], pch=c(17, 19)[COPD.data$SEX], main = "PLS plot of exercise COPD interaction")
 
-
-
+################	explained variance of outcome	################
+yve <- 100 * drop(R2(S4pls, estimate = "train", intercept = FALSE)$val) 
 
 
 
